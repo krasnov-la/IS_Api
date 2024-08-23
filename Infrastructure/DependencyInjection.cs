32b9295bd9da9f0aa694cf10749fc206e4c761d1
Krasnov-la <Leonid.Krasnov.R255@gmail.com>
2024-08-24 01:15:52 +1000

    ef & entities configured

--- Infrastructure/DependencyInjection.cs
+++ Infrastructure/DependencyInjection.cs
@@ -2,8 +2,10 @@
 using Application.Interfaces.Repositories;
 using Infrastructure.Common;
 using Infrastructure.Options.Authentication;
+using Infrastructure.Persistance;
 using Infrastructure.Repositories;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.IdentityModel.Protocols.Configuration;
@@ -13,13 +15,16 @@ public static class DependencyInjection
 {
     public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
     {
-        services.AddRepositories();
+        services.AddPersistance();
         services.AddCommon();
         return services;
     }
 
-    private static IServiceCollection AddRepositories(this IServiceCollection services)
+    private static IServiceCollection AddPersistance(this IServiceCollection services)
     {
+        services.AddDbContext<ISDBContext>(p => p.UseNpgsql(
+            "Server=localhost;Port=5432;Database=postgres;Userid=postgres;Password=SEZAM"));
+
         services.AddScoped<IActivityRepository, ActivityRepository>();
         services.AddScoped<IImageRepository, ImageRepository>();
         services.AddScoped<IRatingRepository, RatingRepository>();
---
+++ Infrastructure/Persistance/EntityConfigs/AchievementEntityTypeConfiguration.cs
@@ -0,0 +1,23 @@
+using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Infrastructure.Persistance.EntityConfigs;
+
+public class AchievementEntityTypeConfiguration
+{
+    public static void Configure(OwnedNavigationBuilder<Request, Achievement> builder)
+    {
+        builder.ToTable("achievements");
+        builder.HasKey(a => a.Id);
+        builder.WithOwner().HasForeignKey("requestId");
+        builder
+            .Property(a => a.Admin)
+            .HasConversion(
+                admin => admin.EmailAddress,
+                address => new Admin(address))
+            .HasMaxLength(128);
+        builder
+            .Property(a => a.Score);
+    }
+}
