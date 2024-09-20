1e463bfe96d20f5a88727fa94044b6f59eb2c0ea
Leonid.Krasnov.R255 <leonid.krasnov.r255@gmail.com>
2024-09-20 12:38:38 +1000

    fixed email extarction from claims

--- Application/Services/ServiceBase.cs
+++ Application/Services/ServiceBase.cs
@@ -5,5 +5,5 @@ namespace Application.Services;
 public abstract class ServiceBase
 {
     public static string ExtractEmail(ClaimsPrincipal user) 
-        => user.FindFirstValue("Email") ?? "aboba"; //TODO throw new ArgumentException("ClaimsPricipal should contain email claim");
+        => user.FindFirstValue(ClaimTypes.Email) ?? throw new ArgumentException("ClaimsPricipal should contain email claim");
 }
